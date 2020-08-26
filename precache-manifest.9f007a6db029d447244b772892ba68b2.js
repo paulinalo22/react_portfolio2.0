187f91776890f6fc89e2fe33efcbd882af33493c
Paulina López Salinas <marselluswallace@Marsellus-Wallace.local>
2020-08-25 22:27:31 -0500

    Updates

---
+++ precache-manifest.9f007a6db029d447244b772892ba68b2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6dfc46de84db0b1614b8c5bd6ea86f",
+    "url": "/react_portfolio2.0/index.html"
+  },
+  {
+    "revision": "80b3f60a1388e3dd0cf4",
+    "url": "/react_portfolio2.0/static/css/main.1739e050.chunk.css"
+  },
+  {
+    "revision": "257e40d10ca58d109cbf",
+    "url": "/react_portfolio2.0/static/js/2.2fee98c5.chunk.js"
+  },
+  {
+    "revision": "d0d6b5033fd77eb6038046252d67d3b7",
+    "url": "/react_portfolio2.0/static/js/2.2fee98c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b3f60a1388e3dd0cf4",
+    "url": "/react_portfolio2.0/static/js/main.1bf5fd69.chunk.js"
+  },
+  {
+    "revision": "e01c993690d1cf469d49",
+    "url": "/react_portfolio2.0/static/js/runtime-main.681efea4.js"
+  },
+  {
+    "revision": "f47636fcf1dd904c5d428047680a4dcb",
+    "url": "/react_portfolio2.0/static/media/Banner_image2.f47636fc.jpg"
+  },
+  {
+    "revision": "7b1bb014c5ba8e701fdacbf1f7ac7996",
+    "url": "/react_portfolio2.0/static/media/Music.7b1bb014.png"
+  },
+  {
+    "revision": "c952c75b2a0b1b6960cb5411dd8c8bf4",
+    "url": "/react_portfolio2.0/static/media/burger.c952c75b.png"
+  },
+  {
+    "revision": "5d9397cc153ef99f339ffb377dde1420",
+    "url": "/react_portfolio2.0/static/media/doggo.5d9397cc.png"
+  },
+  {
+    "revision": "0b9efa6be6560c34372610d6d079358b",
+    "url": "/react_portfolio2.0/static/media/pp.0b9efa6b.png"
+  },
+  {
+    "revision": "322038926ca646ba89c3cd0b8b759336",
+    "url": "/react_portfolio2.0/static/media/resume.32203892.pdf"
+  },
+  {
+    "revision": "538adcd9fc5189b39e2f934fa8d6978f",
+    "url": "/react_portfolio2.0/static/media/school.538adcd9.png"
+  },
+  {
+    "revision": "a93e2cf0efd73fe9487f3d06368d715c",
+    "url": "/react_portfolio2.0/static/media/space.a93e2cf0.mp4"
+  },
+  {
+    "revision": "6052a8372093daf8720ec076378da7bc",
+    "url": "/react_portfolio2.0/static/media/workout.6052a837.png"
+  }
+]);
